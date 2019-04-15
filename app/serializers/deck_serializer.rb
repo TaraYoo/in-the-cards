5c90178018ff5b38719fd7952912235068336ccf
TaraYoo <yootar@gmail.com>
2019-04-15 17:42:09 -0400

    Format cards in deck serializer
    Format cards in deck serializer to show only id, name, icon, and
    meaning based on whether card is up or reversed

--- app/serializers/deck_serializer.rb
+++ app/serializers/deck_serializer.rb
@@ -1,3 +1,23 @@
 class DeckSerializer < ActiveModel::Serializer
-  attributes :id, :question, :accuracy, :readings, :created_at, :updated_at
+  attributes :id, :question, :accuracy
+
+  attribute :formatted_cards do
+    formatted = []
+    object.readings.each do |reading|
+      formatted.push({
+        id: reading.card_id,
+        up: reading.up
+        })
+    end
+    object.cards.each do |card|
+      formatted.each do |format_object|
+        if card.id == format_object[:id]
+          format_object[:name] = card.name
+          format_object[:meaning] = format_object[:up] ? card[:mean_up] : card[:mean_reverse]
+          format_object[:icon] = card.icon
+        end
+      end
+    end
+    formatted
+  end
 end
