b7f12517597191521a15a07b303b19f7d86b307a
TaraYoo <yootar@gmail.com>
2019-04-15 09:18:17 -0400

    Fix linter error

--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,6 +1,6 @@
 class CardSerializer < ActiveModel::Serializer
   attributes :id, :name, :icon
   attribute :meaning do
-    rand < 0.5 ? "#{object.mean_up}" : "#{object.mean_reverse}"
+    rand < 0.5 ? object.mean_up.to_s : object.mean_reverse.to_s
   end
 end
